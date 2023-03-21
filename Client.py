26361a63f221b7dda9208869ea620254983de927
Vojtech Hynek <59179558+xhynek11@users.noreply.github.com>
2023-03-21 09:40:14 +0100

    Update Client.py

--- Client.py
+++ Client.py
@@ -1,19 +1,21 @@
 import socket
+import json
+from Test import KryPiShell
+
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import ec
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 import binascii
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad
+
 from cryptography.hazmat.primitives.serialization import load_der_public_key
 import test_AES
 
 class Client:
     def __init__(self):
-        self.host = '127.0.0.1'
+        self.host = "127.0.0.1"
         self.port = 8080
         self.sock = None
-        self.key = b"gQGP/E2SdzXvyhVoNmdoyvF66uBNZvuq"
+        self.json_data = None
         self.ECDH_key = None
 
     def connect(self):
@@ -23,6 +25,12 @@ class Client:
 
         client.countDHEC()
 
+    def close(self):
+        self.sock.close()
+
+    def add_to_json_data(self, data):
+        self.json_data = json.loads(data)
+        
     def send_data(self, data):
         if self.sock is not None:
             self.sock.send(data)
@@ -41,12 +49,9 @@ class Client:
             data = self.sock.recv(1024)
             return test_AES.decrypt(data, self.ECDH_key)
 
-    def close(self):
-        self.sock.close()
-
     def countDHEC(self):
 
-        #generate private key and converts the ECDH public key to bytes and sends it to the client
+        # generate private key and converts the ECDH public key to bytes and sends it to the client
         Alice_private_key = ec.generate_private_key(ec.SECP384R1())
         Alice_public_key_bytes = Alice_private_key.public_key().public_bytes(
             encoding=serialization.Encoding.DER,
@@ -54,7 +59,7 @@ class Client:
         )
         client.send_data(Alice_public_key_bytes)
 
-        #accepts the client's public key in bytes and convert it to public key object
+        # accepts the client's public key in bytes and convert it to public key object
         Bob_public_key_bytes = self.receive_data()
         Bob_public_key = load_der_public_key(Bob_public_key_bytes)
 
@@ -67,36 +72,29 @@ class Client:
         print("\nBob's derived key: ", binascii.b2a_hex(Alice_derived_key).decode())
 
 
-client = Client()
-client.connect()
+if __name__ == '__main__':
+    client = Client()
+    client.connect()
+
+    client.send_data_AES("hello")
+
+    json_data = client.receive_data_AES()
+    client.add_to_json_data(json_data)
+    try:
+        while True:
+            krypi = KryPiShell()
+            krypi.add_data(json_data)
+            krypi.cmdloop()   
+            data = krypi.retrieve_data()
+            
+            client.send_data(json.dumps(data)) 
+            if input("Press"):
+                break 
+    except KeyboardInterrupt:
+        data = krypi.retrieve_data() 
+        client.send_data(json.dumps(data))
+        print("Exiting....")
 
 
-
-
-while True:
-    message = input("Enter a message to send to the server: ")
-    client.send_data_AES(message)
-
-# HEADER = 64
-# FORMAT = 'utf-8'
-# HOST = '192.168.1.222'
-# PORT =  9090
-# DISCONECT = "disconect"
-
-
-# client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-# client.connect((HOST, PORT))
-
-
-# def send(msg):
-#     message = msg.encode(FORMAT)
-#     msg_len = len(message)
-#     send_len = str(msg_len).encode(FORMAT)
-
-
-# client.send(f"Hello world".encode("utf-8"))
-# print(client.recv(1024))
-
-
+            
+        
